dd92404291a511c7b4af20c3b680c2977151d6a3
Douglas Williams <douglas_wfs@hotmail.com>
2022-02-12 10:21:57 -0300

    Simple Account Create

---
+++ MyFirstProject/MyFirstProject/Program.cs
@@ -0,0 +1,38 @@
+﻿using MyFirstProject.User;
+using System;
+using System.Globalization;
+
+
+namespace MyFirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("--- CADASTRO DE CLIENTES ---");
+            Console.Write("Quantos clientes você deseja cadastrar no sistema? ");
+            int users = int.Parse(Console.ReadLine());
+
+            AccountCreate[] accounts = new AccountCreate[users];
+
+            for (int i = 0; i < users; i++)
+            {
+                Console.WriteLine($"Usuário #{i+1}: ");
+                Console.Write("Nome Completo: ");
+                string name = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Data de nascimento(Dia/Mês/Ano): ");
+                string date = Console.ReadLine();
+                accounts[i] = new AccountCreate { Name = name, Email = email, Date = date };
+
+            }
+            for (int i = 0; i < users; i++)
+            {
+                Console.WriteLine($"Id:0{i + 1}" + " - " + accounts[i].ToString());
+            }
+
+        }
+    }
+}
---
+++ MyFirstProject/MyFirstProject/User/AccountCreate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProject.User
+{
+    public class AccountCreate
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Date { get; set; }
+
+        public AccountCreate()
+        {
+        }
+
+        public AccountCreate(string name, string email, string date)
+        {
+            Name = name;
+            Email = email;
+            Date = date;
+        }
+        public override string ToString()
+        {
+            return Name + ", " + Email + ", " + Date;
+        }
+    }
+}
